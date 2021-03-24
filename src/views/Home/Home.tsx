c525388ccbfe81a92672870a7eb18e08dd4dac2c
kent <gkh1974@gmail.com>
2021-03-23 23:35:11 -0400

    adding initial files

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Heading, Text, BaseLayout } from '@jurassicfarm/jurassic-uikit'
+import useI18n from 'hooks/useI18n'
+import Page from 'components/layout/Page'
+import FarmStakingCard from './components/FarmStakingCard'
+// import LotteryCard from './components/LotteryCard'
+import BlzdStats from './components/BlzdStats'
+import TotalValueLockedCard from './components/TotalValueLockedCard'
+// import TwitterCard from './components/TwitterCard'
+
+const Hero = styled.div`
+  align-items: center;
+  background-image: url('/images/blzd/3.png');
+  background-size: 110px;
+  background-repeat: no-repeat;
+  background-position: top center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  margin-bottom: 32px;
+  padding-top: 116px;
+  text-align: center;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/red-dino.png'), url('/images/green-dino.png');
+    background-size: 150px;
+    background-position: left center, right center;
+    height: 165px;
+    padding-top: 0;
+  }
+`
+
+const Cards = styled(BaseLayout)`
+  align-items: stretch;
+  justify-content: stretch;
+  margin-bottom: 32px;
+
+  & > div {
+    grid-column: span 6;
+    width: 100%;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    & > div {
+      grid-column: span 8;
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    & > div {
+      grid-column: span 6;
+    }
+  }
+`
+
+const Home: React.FC = () => {
+  const TranslateString = useI18n()
+
+  return (
+    <Page>
+      <Hero>
+        <Heading as="h1" size="xl" mb="24px" color="primary">
+          {TranslateString(576, 'JurassicFarm')}
+        </Heading>
+        <Text>{TranslateString(578, 'The best DEFI app on Binance Smart Chain.')}</Text>
+      </Hero>
+      <div>
+        <Cards>
+          <FarmStakingCard />
+          <BlzdStats />
+        </Cards>
+      </div>
+      <TotalValueLockedCard />
+      {/* <TwitterCard/> */}
+    </Page>
+  )
+}
+
+export default Home
